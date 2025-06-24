a672acc54f567a3f48b644cbdb2120aee4d8718c
Sushen Kolakaleti <yamunasoftware18@gmail.com>
2025-06-24 19:42:32 -0400

    Updated Temp File Removal

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ import os
 
 ### SETUP ###
 
+# Initialize Flask App:
 os.environ['MANAGE'] = 'FALSE'
 app = Flask(__name__)
 
+# Clears Server Log:
 def clear_log(filename):
   with open(filename, 'w') as file:
     pass
     file.write('--- SERVER LOG ---\n\n')
 
+# Generates Unique File Signature:
 def generate_signature():
   signature = ''
   for _ in range(20):
     signature += str(random.randint(0, 9))
   return signature
 
+# Converts Video to Standard Playback Format:
 def convert_video(filename):
   input_file = 'media/' + filename
   output_file = 'media/staged_' + filename
@@ -40,6 +44,10 @@ def convert_video(filename):
 
   os.remove(input_file)
   os.rename(output_file, input_file)
+
+  for file in os.listdir('media'):
+    if '.tmp' in file:
+      os.remove('media/' + file)
 
 ### SERVICES ###
 
